9ad29b3ba53a08d2817eceefbcaab2527c413b40
wxShawn <45098074+wxShawn@users.noreply.github.com>
2022-08-06 16:39:31 +0800

    上传本地项目

---
+++ README.md
@@ -0,0 +1,10 @@
+# Markdown editor component
+## 介绍
+这是一个基于 vue.js 和 marked.js 开发的 markdown 编辑器组件。（水平较低，大佬请见谅）
+## 截图
+![组件截图](https://raw.githubusercontent.com/wxShawn/media-lib/main/images/markdown-editor-sc.png)
+## 功能
+- [x] 实时预览
+- [x] 全屏显示
+- [x] 快捷添加表格、列表等
+- [x] 自动识别文章标题
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import MarkdownEditor from './components/MarkdownEditor.vue';
+</script>
+
+<template>
+  <markdown-editor></markdown-editor>
+</template>
+
+<style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  width: 800px;
+  height: 500px;
+  padding: 100px;
+}
+</style>
